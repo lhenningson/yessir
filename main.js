65e1b1f08b1be371c60f35bc582a6abcf1d50153
lhenningson <l.henningson@share.epsb.ca>
2022-09-28 11:23:13 -0600

    yessir

--- main.js
+++ main.js
@@ -4,5 +4,30 @@
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  console.log("Search");
+  // Input
+  let name = document.getElementById("char-in").value;
+
+  //If Statement
+  if (name === "Po") {
+    document.getElementById("char-name").innerHTML = "Po";
+    document.getElementById("char-quote").innerHTML = "Buddy, I am the Dragon Warrior!";
+    document.getElementById("char-img").src = "img/po.png";
+  } else if (name === "Tigress") {
+    document.getElementById("char-name").innerHTML = "Tigress";
+    document.getElementById("char-quote").innerHTML = "That was hardcore!";
+    document.getElementById("char-img").src = "img/tigress.png";
+  } else if (name === "Mantis") {
+    document.getElementById("char-name").innerHTML = "Mantis";
+    document.getElementById("char-quote").innerHTML = "Fear the Bug!";
+    document.getElementById("char-img").src = "img/mantis.png";
+  } else if (name === "Monkey") {
+    document.getElementById("char-name").innerHTML = "Monkey";
+    document.getElementById("char-quote").innerHTML = "Ooh Ah Ah!";
+    document.getElementById("char-img").src = "img/monkey.png";
+  } else {
+    document.getElementById("char-name").innerHTML = "??????";
+    document.getElementById("char-quote").innerHTML = "-----------";
+    document.getElementById("char-img").src = "img/question-mark.png";
+  }
 }
+  
